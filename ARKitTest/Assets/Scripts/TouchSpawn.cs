85efb16ee5962aad1186ed44135cd78c51d045d0
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-08-09 04:44:51 +0900

    レイを飛ばして当たった座標を取得するようにした。

--- ARKitTest/Assets/Scripts/TouchSpawn.cs
+++ ARKitTest/Assets/Scripts/TouchSpawn.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using System.Collections.Generic;
 
 namespace ARKitTest
 {
     /// <summary>
     /// タッチでオブジェクトを生成する。
     /// </summary>
+    [RequireComponent(typeof(ARPlaneManager))]
     [RequireComponent(typeof(ARRaycastManager))]
     public class TouchSpawn : MonoBehaviour
     {
@@ -17,6 +19,18 @@ namespace ARKitTest
         protected void Awake()
         {
             Raycast = GetComponent<ARRaycastManager>();
+        }
+
+        protected void Update()
+        {
+            if (Input.touchCount == 0) { return; }
+
+            Touch Info = Input.GetTouch(0);
+            if (Info.phase != TouchPhase.Began) { return; }
+
+            List<ARRaycastHit> Hits = new List<ARRaycastHit>();
+            if (!Raycast.Raycast(Info.position, Hits)) { return; }
+            Debug.Log(Hits[0].pose.position);
         }
     }
 }
